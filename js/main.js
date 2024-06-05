be0d974cdc175ce6a49612078087af62f63651cc
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-05 22:52:44 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const genera = document.getElementById("conf");
 const annulla = document.getElementById("ann");
 const attiva = document.querySelector(".calc_prezzo");
 let prezzoKm = 0.21;
+let nCarrozza = Math.floor(Math.random() * 10);
+let nBiglietto = Math.floor(Math.random() * 1000);
 
 // evento click preleva dati
 // calcola il prezzo
@@ -15,20 +17,27 @@ genera.addEventListener("click",
         let valoreNome = nome.value;
         let valoreKm = parseInt(kmPercorsi.value) ;
         let valoreEta= eta.value;
-        if (valoreEta === "18-"){
+        
+        if (valoreEta === "minorenne"){
             prezzoKm = (0.21- ((0.21*20)/100));
             
         } else if (valoreEta === "over65" ){
             prezzoKm = (0.21- ((0.21*40)/100));
         }
+        
         console.log("nome: " + valoreNome,"età: " + valoreEta, "km: " + valoreKm);
         let risultato = (prezzoKm * valoreKm).toFixed(2);
         console.log("prezzo biglietto: " + risultato);
+        document.getElementById("titolo").innerHTML = valoreNome;
+        document.getElementById("tariffa").innerHTML = valoreEta;
+        document.getElementById("carrozza").innerHTML = nCarrozza ;
+        document.getElementById("n_bigli").innerHTML = nBiglietto;
+        document.getElementById("prezzo").innerHTML = risultato;
+
     }
 )
 
 // stampa risultato biglietto
-
 
 
 // refresh al click annulla
